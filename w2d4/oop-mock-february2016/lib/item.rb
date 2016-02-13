950f1c2f05d9d7ba325c9fd97d5ba45e84779244
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-02-13 03:59:23 +0000

    remove mock test

--- w2d4/oop-mock-february2016/lib/item.rb
+++
@@ -1,9 +0,0 @@
-class Item
-
-  attr_accessor :name, :weight
-
-  def initialize(name, weight)
-    @name = name
-    @weight = weight
-  end
-end
--- w2d4/oop-mock-february2016/lib/plasma_cannon.rb
+++
@@ -1,6 +0,0 @@
-class PlasmaCannon < Weapon
-  def initialize
-    super("Plasma Cannon", 200, 55)
-  end
-
-end
