c93938c191c8810199fdfe4ff8d4c6779e554d37
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-02-13 04:45:55 +0000

    w2d3 scraper add colorize

--- w2d3/main.rb
+++ w2d3/main.rb
@@ -1,6 +1,7 @@
 require_relative 'post'
 require_relative 'comment'
 require_relative 'scraper'
+require 'colorize'
 
 class Scraper
   #doc = Nokogiri::HTML(File.open('post.html'))
@@ -29,10 +30,10 @@ class Scraper
       first_post.add_comment(comment_object)
     end
 
-    puts "Post title: #{first_post.title}"
-    puts "Post url is #{first_post.url}"
-    puts "This post has #{first_post.points}"
-    puts "Number of comments: #{comments.size}"
+    puts "Post title: #{first_post.title}".red
+    puts "Post url is #{first_post.url}".green
+    puts "This post has #{first_post.points}".yellow
+    puts "Number of comments: #{comments.size}".blue
 
     #puts first_post.comments[0].content
   else
